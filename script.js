e1a131fd7461faa371df209091279c4d751f44a7
Daniel Nijdl <dnijdl276@west-mec.org>
2020-09-11 13:18:47 -0700

    Added hexadecimal converter

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let decimal = 178 //prompt(`Enter a decimal number between 0 and 255.`);
+let decimal = prompt(`Enter a decimal number between 0 and 255.`);
 let decimalforbinary = decimal;
 
+//binary
 
 let binary128 = 0;
 let binary64 = 0;
@@ -50,4 +51,77 @@ if(decimal >= 1)
     decimal -= 1;
     binary1 = 1;
 }
-console.log(`${binary128}${binary64}${binary32}${binary16} ${binary8}${binary4}${binary2}${binary1}`);
+
+//hexidecimal
+
+rem1 = decimalforbinary % 16;
+dec1 = decimalforbinary / 16;
+rem2 = Math.floor(dec1) % 16
+
+//remainder1
+
+if(rem1 == 10) {
+    rem1 = "A";
+
+}
+
+if(rem1 == 11) {
+    rem1 = "B";
+    
+}
+
+if(rem1 == 12) {
+    rem1 = "C";
+    
+}
+
+if(rem1 == 13) {
+    rem1 = "D";
+    
+}
+
+if(rem1 == 14) {
+    rem1 = "E";
+    
+}
+
+if(rem1 == 15) {
+    rem1 = "F";
+    
+}
+
+//remainder 2
+
+if(rem2 == 10) {
+    rem2 = "A";
+    
+}
+
+if(rem2 == 11) {
+    rem2 = "B";
+    
+}
+
+if(rem2 == 12) {
+    rem2 = "C";
+    
+}
+
+if(rem2 == 13) {
+    rem2 = "D";
+    
+}
+
+if(rem2 == 14) {
+    rem2 = "E";
+    
+}
+
+if(rem2 == 15) {
+    rem2 = "F";
+    
+}
+
+console.log(`The binary of ${decimalforbinary} is ${binary128}${binary64}${binary32}${binary16} ${binary8}${binary4}${binary2}${binary1}`);
+
+console.log(`The hexadecimal of ${decimalforbinary} is ${rem2}${rem1}`);
